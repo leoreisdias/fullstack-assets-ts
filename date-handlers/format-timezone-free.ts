d526d04e79b75c566a09547056fd848cd3fdb81d
Leonardo R. Dias <47978193+leoreisdias@users.noreply.github.com>
2023-10-24 17:22:23 -0300

    Get Date in Specific TimeZone

--- date-handlers/format-timezone-free.ts
+++ date-handlers/format-timezone-free.ts
@@ -24,3 +24,31 @@ export const formatDate = (
     locale: ptBR,
   });
 };
+
+/* 
+Main TimeZones: 
+America/New_York
+America/Los_Angeles
+America/Chicago
+America/Denver
+America/Toronto
+Europe/London
+Europe/Paris
+Europe/Berlin
+Asia/Tokyo
+Asia/Shanghai
+Australia/Sydney
+Africa/Cairo
+Pacific/Honolulu 
+
+Others: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  */
+
+export const getDateAmericaSP = (): Date => {
+    const today = new Date();
+    const timezone = 'America/Sao_Paulo';
+
+    const desiredDate = utcToZonedTime(today, timezone);
+
+    return desiredDate;
+};
