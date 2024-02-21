776f93049e8b6c7fee3979d658bbfba86f2f3cd7
Leonardo R. Dias <47978193+leoreisdias@users.noreply.github.com>
2024-02-21 00:17:06 +0000

    fix: fetcher type response

--- nextjs-app-router/fetcher-sender.ts
+++ nextjs-app-router/fetcher-sender.ts
@@ -19,43 +19,57 @@ export const fetcher = async <T = unknown>(
   
   const baseUrl = process.env.BASE_URL; // Example, but I recommend an env variable approach
 
-  const res = await fetch(`${baseUrl}${input}`, {
-    ...init,
-    headers: {
-      ...(init?.headers ?? {}),
-      Authorization: `Bearer ${token}`,
-    },
-  });
+  try{
+    const res = await fetch(`${baseUrl}${input}`, {
+      ...init,
+      headers: {
+        ...(init?.headers ?? {}),
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-  if (!res.ok) {
-    console.error('fetcher:url:', input, 'init:', init, 'res:', res);
-    
-    if (res.status === 401) {
-      return redirect(`/sign-out`); // NextAuth Use Case: A custom page to call the server-action signOut from next-auth middleware - Adapt for your use case
+    if (!res.ok) {
+      console.error('fetcher:url:', input, 'init:', init, 'res:', res);
+      
+      if (res.status === 401) {
+        return redirect(`/sign-out`); // NextAuth Use Case: A custom page to call the server-action signOut from next-auth middleware - Adapt for your use case
+      }
+
+     return {
+        isSuccess: false,
+        message: 'An error occurred while fetching the data.',
+        payload: null,
+      } as TResponse<null>;
     }
 
-    throw new Error('An error occurred while fetching the data.');
-  }
+    try {
+      const data = await res.json();
 
-  try {
-    const data = await res.json();
+      return data as TResponse<T>;
+    } catch (err) {
+      console.error(
+        'fetcher:url:',
+        `${baseUrl}${input}`,
+        'init:',
+        init,
+        'res:',
+        res,
+        err,
+      );
 
-    return data as TResponse<T>;
-  } catch (err) {
-    console.error(
-      'fetcher:url:',
-      `${baseUrl}${input}`,
-      'init:',
-      init,
-      'res:',
-      res,
-      err,
-    );
+      return {
+        isSuccess: false,
+        message: 'An error occurred while fetching the data.',
+        payload: null,
+      } as TResponse<null>;
+    }
+  }catch(err){
+    console.error('fetcher:url:', `${baseUrl}${input}`, 'init:', init, err);
     return {
       isSuccess: false,
       message: 'An error occurred while fetching the data.',
       payload: null,
-    } as TResponse<T>;
+    } as TResponse<null>;
   }
 };
 
