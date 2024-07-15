d74f291a71c3dbc0bd0f525d8946ad8647dc756f
Leonardo R. Dias <47978193+leoreisdias@users.noreply.github.com>
2024-07-15 22:38:13 +0000

    fix: date revalidator and debounce function

--- @nextjs-app-router/server-stale-while-revalidate/DateRevalidator.tsx
+++ @nextjs-app-router/server-stale-while-revalidate/DateRevalidator.tsx
@@ -1,54 +1,75 @@
+"use client";
 // Font: https://github.com/vercel/next.js/discussions/54075
 
-import { useRouter, usePathname } from 'next/navigation'
-import { debounce } from 'lodash';
+import { debounce } from "@/utils/functions/debounce";
+import { useRouter, usePathname } from "next/navigation";
+import { useCallback, useEffect } from "react";
 
 interface DataRevalidatorProps {
   /**
    * The timeout in milliseconds to wait before revalidating the data.
    */
   timeout?: number;
+
+  /**
+   * The interval in milliseconds to revalidate the data.
+   */
+  revalidateInterval?: number;
 }
 
-export function DataRevalidator({
-  timeout = 5000,
-}: DataRevalidatorProps): null {
+export function DataRevalidator({ timeout = 5000, revalidateInterval }: DataRevalidatorProps): null {
   const router = useRouter();
-  const pathname = usePathname()
+  const pathname = usePathname();
 
-  const getRefresher = useCallback(() => debounce(
-    () => {
-      // Can be removed, just to showcase when it triggers refresh
-      console.log(
-        `[${new Date().toLocaleTimeString()}] Triggering router.refresh()...`,
-      );
-      router.refresh();
-    },
-    timeout,
-		{ leading: false, trailing: true }
-  ), [router, timeout]);
+  const getRefresher = useCallback(
+    () =>
+      debounce(
+        () => {
+          // Can be removed, just to showcase when it triggers refresh
+          console.info(`[${new Date().toLocaleTimeString()}] Triggering refresh...`);
+          router.refresh();
+        },
+        timeout,
+        { leading: false, trailing: true },
+      ),
+    [router, timeout],
+  );
 
   // Trigger router.refresh() whenever `pathname` changes.
-	// Note: this does not cover searchParams, you can add `useSearchParams` if needed.
-  useEffect(()=> {
-		const refresher = getRefresher()
-    refresher()
-	}, [revalidate, pathname])
+  // Note: this does not cover searchParams, you can add `useSearchParams` if needed.
+  useEffect(() => {
+    const refresher = getRefresher();
+    refresher();
+  }, [pathname, getRefresher]);
 
   // Trigger router.refresh() when focus goes back to the window (similar to useSWR / react-query)
   useEffect(() => {
-    if(process.env.NODE_ENV === 'development') {
-			return
+    if (process.env.NODE_ENV === "development") {
+      return;
     }
 
-		const refresher = getRefresher()
+    const refresher = getRefresher();
 
-    window.addEventListener('focus', refresher);
+    window.addEventListener("focus", refresher);
     return () => {
       refresher.cancel();
-      window.removeEventListener('focus', refresher);
+      window.removeEventListener("focus", refresher);
     };
-  }, [revalidate]);
+  }, [getRefresher]);
+
+  // Trigger router.refresh() at a fixed interval
+  useEffect(() => {
+    if (!revalidateInterval) {
+      return;
+    }
+
+    const refresher = getRefresher();
+
+    const interval = setInterval(refresher, revalidateInterval);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [revalidateInterval, getRefresher]);
 
   return null;
 }
