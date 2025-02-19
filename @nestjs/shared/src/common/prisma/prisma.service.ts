d5188bd4e2cdb152458c45dbf01af3203b1625ff
Leonardo R. Dias <47978193+leoreisdias@users.noreply.github.com>
2025-02-19 17:56:04 +0000

    feat: prisma comments

---
+++ @nestjs/shared/src/common/prisma/prisma.service.ts
@@ -0,0 +1,50 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { formatDate } from '../utils/date';
+
+// NOTE: IN CASE YOU WANT TO $extend the client and create custom method, columns (virtuals), and so on... 
+// https://www.prisma.io/docs/orm/prisma-client/client-extensions
+const prismaExtendedClient = (prismaClient: PrismaClient) =>
+  prismaClient.$extends({
+    model: {
+      // ...
+    },
+  });
+
+type ExtendedPrismaClient = ReturnType<typeof prismaExtendedClient>;
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  readonly query: ExtendedPrismaClient = prismaExtendedClient(this); // for custom methods and attributes
+
+  constructor() {
+    super();
+
+    return new Proxy(this, {
+      get: (target, property) =>
+        Reflect.get(property in this.query ? this.query : target, property),
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
+
+/*
+Usage Example:
+
+For default operations:
+  `this.prismaService.myTable.findMany()`
+
+Or for extended features:
+  `this.prismaService.query.myTable.customMethod()`
+
+*/
