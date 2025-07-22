2b0527460f602404916689a5232037b600bb4a0b
Leonardo Reis <leonardord99@gmail.com>
2025-07-22 11:43:56 -0300

    fix: update clietFetcher null Result payload

--- react/@api/api-fetch.ts
+++ react/@api/api-fetch.ts
@@ -3,14 +3,14 @@ import { Result } from "../@types/response";
 const api = axios.create();
 
 // NOTE: USAGE TIP - Good for _queryFn_ from _useQuery_ hook (Tanstack Query)
+// NOTE: CONSIDER THAT DATA CAN BE UNDEFINED AND THE PAYLOAD FROM RESULT CAN BE NULL -> AVOID ERROR BOUNDARIES AS MUCH AS POSSIBLE
 
-export const clientFetcher = async <T = any>(
+export const clientFetcher = async <T = any, W = Result<T | null>>(
   url: string,
   config?: AxiosRequestConfig<T> | undefined
 ) => {
-  const res = await api.get<Result<T>>(url, {
+  const res = await api.get<W>(url, {
     baseURL: "/api/internal",
-
     ...config,
   });
 
